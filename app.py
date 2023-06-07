38d19b24d661c9dfdff3ed84ef40ed1f0d74cec2
Your Name <you@example.com>
2023-06-07 22:23:41 +0300

    wip refactor + show app icon

--- app.py
+++ app.py
@@ -1,102 +1,92 @@
-# import win32api
-# from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY
-import jsonpickle
-import asyncio
-import uuid
+import base64
 
 from flask import Flask, request, send_from_directory
 from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 
 app = Flask(__name__)
 
-mapping = {}
+global_sessions = list()
 
-def get_key(index):
-    return str(index)
+def get_key(dict):
+    message = u' '.join((dict['artist'], dict['title'])).encode('utf-8').strip()
+    base64_bytes = base64.urlsafe_b64encode(message)
+    return base64_bytes.decode('ascii')
+
+import subprocess
+
+def get_proc_name(AppId):
+    cmd = f"get-StartApps | Where-Object {{$_.AppId -like '{AppId}'}} | Select Name | foreach {{ $_.Name }}"
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+
+    if (completed.returncode != 0):
+        return ''
+    return completed.stdout.decode('ascii').replace('\r\n', '')
 
 async def get_media_info():
     sessions = await MediaManager.request_async()
-    x = list()
-    index = 0
+
     for item in sessions.get_sessions():
         if item:
+            obj = {}
             info = await item.try_get_media_properties_async()
             info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+            info1 = {}
+            info1['title'] = info_dict['title']
+            info1['artist'] = info_dict['artist']
 
-            new_dict = {}
-            new_dict['title'] = info_dict['title']
-            new_dict['artist'] = info_dict['artist']
-            new_dict['index'] = index
-            new_dict['status'] = 'paused'
+            id = get_key(info1)
+            info1['id'] = id
+            obj['id'] = id
 
-            if get_key(index) not in mapping:
-                id = str(uuid.uuid4())
-                mapping[get_key(index)] = id
-            new_dict['id'] = mapping.get(get_key(index))
-            index = index + 1
+            info1['status'] = 'paused'
 
             if item.get_playback_info().playback_status == item.get_playback_info().playback_status.PLAYING:
-                new_dict['status'] = 'playing'
+                info1['status'] = 'playing'
 
-            x.append(new_dict)
-    return x
+            obj['info'] = info1
+            obj['item'] = item
+
+            index = next((index for (index, item) in enumerate(global_sessions) if item["id"] == obj['id']), -1)
+            obj['info']['index'] = index
+            obj['info']['source_app'] = get_proc_name(item.source_app_user_model_id)
+            if (index == -1):
+                obj['info']['index'] = 0
+                global_sessions.insert(0, obj)
+            else:
+                global_sessions[index] = obj
+    return global_sessions
 
 @app.route("/")
 def root():
     return send_from_directory('static', 'index.html')
 
 @app.route("/sessions")
-def hello_world():
-    current_media_info = asyncio.run(get_media_info())
-    return jsonpickle.decode(jsonpickle.encode(current_media_info))
+async def sessions():
+    data = await get_media_info()
+    return list(map(lambda x: x['info'], list(data)))
 
 @app.route("/toggle")
 async def toggle():
-    args = request.args
-    id = args.get("id")
-    action = args.get('action')
-    sessions = await MediaManager.request_async()
-    x = list()
+    id = request.args.get("id")
+    action = request.args.get('action')
+    data = await get_media_info()
+    item = next((item for item in data if item["id"] == id), None)
 
     if id is None:
+        return 400
+    
+    if item == None:
         return 404
-    
-    index = 0
-    
-    for item in sessions.get_sessions():
-        if item:
-            info = await item.try_get_media_properties_async()
-            info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
-            idd = mapping.get(get_key(index))
-            new_dict = {}
-            new_dict['title'] = info_dict['title']
-            new_dict['artist'] = info_dict['artist']
-            new_dict['id'] = idd
-            new_dict['index'] = index
 
-            is_playing = item.get_playback_info().playback_status == item.get_playback_info().playback_status.PLAYING
+    handle = item['item']
 
-            if (is_playing):
-                new_dict['status'] = 'playing'
-            else:
-                new_dict['status'] = 'paused'
+    if action:
+        if action == 'prev':
+            await handle.try_skip_previous_async()
+        if action == 'next':
+            await handle.try_skip_next_async()
+    else:
+        handle.try_toggle_play_pause_async()
 
-            if id == idd:
-                if action:
-                    if action == 'prev':
-                        await item.try_skip_previous_async()
-                    if action == 'next':
-                        await item.try_skip_next_async()
-                    return '{"ok":true}'
-                else:
-                    if is_playing:
-                        new_dict['status'] = 'paused'
-                        await item.try_pause_async()
-                    else:
-                        new_dict['status'] = 'playing'
-                        await item.try_play_async()
-
-            index = index + 1
-            x.append(new_dict)
-
-    return jsonpickle.decode(jsonpickle.encode(x))
+    data = await get_media_info()
+    return list(map(lambda x: x['info'], list(data)))
